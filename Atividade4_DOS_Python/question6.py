8fcb9897fc07705247ca52474b9c716413238f71
Rangerson Clemente <rangerson14@outlook.com>
2024-03-14 06:58:57 -0300

    Update question6.py

--- Atividade4_DOS_Python/question6.py
+++ Atividade4_DOS_Python/question6.py
@@ -1,19 +1,32 @@
 # Faça um Programa que peça as quatro notas de 10 alunos, calcule e armazene num 
 # vetor a média de cada aluno, imprima o número de alunos com média maior ou igual a 7.0.
 
-nota1 = [6,8,5,8.5,4,6,9,8,9,10]
-nota2 = [6,7,5,8,8,9,7,8.5,7,3.5]
-nota3 = [5.7,7,5,8,8,9,10,8,8,7]
-nota4 = [7,2,5,7,7,3,6,9,7,8]
+nota1 = list()
+nota2 = list()
+nota3 = list()
+nota4 = list()
+nota = list()
 
 mediaNotas = []
+qtdAlunos = 10
 contarNotaMaior7 = 0
 
+for i in range(qtdAlunos):
+    print(f" **\n Notas do aluno {i+1} **\n")
+    for i in range(4):
+        nota.append(float(input(f"Informe a {i+1}* nota: ")))
+
+    nota1.append(nota[0])
+    nota2.append(nota[1])
+    nota3.append(nota[2])
+    nota4.append(nota[3])
+    nota.clear()
+
 for i in range(len(nota1)):
-    print(i)
-    media = nota1[i]+nota2[i]+nota3[i]+nota4[i]/4
+    media = (nota1[i]+nota2[i]+nota3[i]+nota4[i])/4
     mediaNotas.append(media)
-    
+
+print(mediaNotas+"\n")
 for i in mediaNotas:
     if i >= 7:
         contarNotaMaior7 +=1
